557b022405c49f70a51c3d87f8530692ae735cd4
djf1991 <947407533@qq.com>
2022-01-17 21:05:04 +0800

    init

---
+++ src/main/java/com/jeff/stylecode/StyleCodeApplication.java
@@ -0,0 +1,13 @@
+package com.jeff.stylecode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StyleCodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StyleCodeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jeff/stylecode/StyleCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeff.stylecode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StyleCodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
